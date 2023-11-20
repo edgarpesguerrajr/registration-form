c09307c0f662c8b428a5acad110666550b839119
Edgar P. Esguerra Jr <jedesguerra37@gmail.com>
2023-11-20 13:27:35 +0800

    connect the .php to .css

--- php/index.php
+++ php/index.php
@@ -16,7 +16,7 @@ else{
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Index</title>
-  <link rel="stylesheet" href="css\index.css">
+  <link rel="stylesheet" href="../css/index.css">
 </head>
 <body>
   <h1>Welcome <?php echo $row["name"]; ?> your email is <?php echo $row["email"]; ?></h1>
--- php/login.php
+++ php/login.php
@@ -31,7 +31,7 @@ if(isset($_POST["submit"])){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="css\login.css">
+  <link rel="stylesheet" href="../css/login.css">
 </head>
 <body>
 <h2>Login</h2>
