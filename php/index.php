dacbb74644a29d46fd62b38c7082e7e9996a1893
Edgar P. Esguerra Jr. <jedesguerra37@gmail.com>
2023-11-20 12:15:50 +0800

    first commit

---
+++ php/index.php
@@ -0,0 +1,25 @@
+<?php
+require 'config.php';
+if(!empty($_SESSION["id"])){
+  $id = $_SESSION["id"];
+  $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $id");
+  $row = mysqli_fetch_assoc($result);
+}
+else{
+  header("Location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Index</title>
+  <link rel="stylesheet" href="css\index.css">
+</head>
+<body>
+  <h1>Welcome <?php echo $row["name"]; ?> your email is <?php echo $row["email"]; ?></h1>
+  <a href="logout.php">logout</a>
+</body>
+</html>
