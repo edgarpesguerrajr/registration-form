5f72195a9bb0343578a7c389afd25a96c8a9a58d
Edgar P. Esguerra Jr <jedesguerra37@gmail.com>
2023-11-21 12:55:54 +0800

    added js files
    remove the script in the php file then added a js file and connected it to the php file

---
+++ js/registration.js
@@ -0,0 +1,14 @@
+function togglePasswordVisibility(inputId, iconId) {
+  var passwordInput = document.getElementById(inputId);
+  var eyeIcon = document.getElementById(iconId);
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  }
+}
--- php/login.php
+++ php/login.php
@@ -55,22 +55,6 @@ if(isset($_POST["submit"])){
   </form>
   <br>
   <a href="registration.php">Register</a>
-
-  <script>
-    function togglePasswordVisibility() {
-      var passwordInput = document.getElementById('password');
-      var eyeIcon = document.getElementById('eye-icon');
-
-      if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        eyeIcon.classList.remove('fa-eye-slash');
-        eyeIcon.classList.add('fa-eye');
-      } else {
-        passwordInput.type = 'password';
-        eyeIcon.classList.remove('fa-eye');
-        eyeIcon.classList.add('fa-eye-slash');
-      }
-    }
-  </script>
 </body>
+<script src="../js/login.js"></script>
 </html>
